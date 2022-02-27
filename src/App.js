ae423a4934678be2c3d0d6e5b64794e68bb44d96
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-27 13:48:19 +0100

    add keyboard clicks handler, add current action state

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import audioData from "./audio";
 function App() {
   const [volumeLevel, setVolumeLevel] = React.useState(0.2);
   const [currentBank, setCurrentBank] = React.useState("bankOne");
+  const [currentAction, setCurrentAction] = React.useState("");
 
+  console.log(currentAction);
   function updateBank() {
     setCurrentBank((oldBank) =>
       oldBank === "bankOne" ? "bankTwo" : "bankOne"
@@ -19,21 +21,41 @@ function App() {
     setVolumeLevel(newVolumeLevel);
   }
 
-  function playAudio(audioURL) {
+  function playAudio(audioURL, audioName) {
     const audioObj = new Audio(audioURL);
+    console.log(audioObj);
     audioObj.volume = volumeLevel;
     audioObj.play();
+    setCurrentAction(audioName);
+  }
+  React.useEffect(() => {
+    window.addEventListener("keyup", handleKeyPress);
+    return () => {
+      window.removeEventListener("keyup", handleKeyPress);
+    };
+  });
+
+  function handleKeyPress({ keyCode }) {
+    audioData[currentBank].forEach((drum) => {
+      if (drum.keyCode === keyCode) {
+        playAudio(drum.url, drum.id);
+      }
+    });
   }
 
   const drumButtons = audioData[currentBank].map((drum) => (
     <DrumButton
-      audioName={drum.id}
-      playAudio={() => playAudio(drum.url)}
+      playAudio={() => playAudio(drum.url, drum.id)}
       keyTrigger={drum.keyTrigger}
       keyCode={drum.keyCode}
     />
   ));
-  return <div>{drumButtons}</div>;
+  return (
+    <div>
+      <h1>{currentAction}</h1>
+      {drumButtons}
+    </div>
+  );
 }
 
 export default App;
