67d2ed213477cb702822c1106b7c1e0783c40967
Leda <38331868+HypnotizedLeda@users.noreply.github.com>
2022-02-28 15:09:14 +0100

    change class of button when pressed

--- src/components/DrumButton.js
+++ src/components/DrumButton.js
@@ -3,7 +3,9 @@ import React from "react";
 import "../styles/DrumButton.css";
 export default function DrumButton(props) {
   const classNames =
-    props.currentButton === props.audioName ? "drum-button red" : "drum-button";
+    props.currentButton === props.audioName
+      ? "drum-button pressed-button"
+      : "drum-button";
   return (
     <button type="button" className={classNames} onClick={props.playAudio}>
       {props.keyTrigger}
